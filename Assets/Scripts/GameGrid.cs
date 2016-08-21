a791396a9bd757d896a9ff35c08d03e7a23d3054
Fede <ftedinur@itba.edu.ar>
2016-08-20 22:33:06 -0300

    created game grid

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGrid : MonoBehaviour {
+
+    public int gridSize = 5;
+    public GameObject gridCubeClone;
+
+    public void SetupGrid() {
+        if (gridSize % 2 == 0) {
+            gridSize++;
+        }
+
+        gridSize = Mathf.Max(gridSize, 5);
+
+        float finalGridSize = gridCubeClone.transform.transform.localScale.x * gridSize;
+        float halfGridSize = finalGridSize / 2;
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                for (int k = 0; k < gridSize; k++) {
+
+                    // Dont add cubes at center of 3d grid
+                    if ((k != 0 && k != gridSize - 1) && (j != 0 && j != gridSize - 1) && (i != 0 && i != gridSize - 1)) {
+                        continue;
+                    }
+
+                    GameObject cube = Instantiate(gridCubeClone);
+                    cube.transform.SetParent(transform);
+
+                    Vector3 size = cube.transform.localScale;
+                    float offset = halfGridSize - size.x / 2;
+                    cube.transform.Translate(i * size.x - offset, j * size.x - offset, k * size.x - offset);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/GridCube.cs
+++ Assets/Scripts/GridCube.cs
@@ -3,10 +3,37 @@ using System.Collections;
 
 public class GridCube : MonoBehaviour {
 
+    public enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+    public GridCube GetNextCube(Direction dir) {
+        RaycastHit hit;
+        Vector3 direction;
+        switch (dir) {
+            case Direction.UP:
+                direction = new Vector3(0, 1, 0);
+                break;
+            case Direction.DOWN:
+                direction = new Vector3(0, -1, 0);
+                break;
+            case Direction.LEFT:
+                direction = new Vector3(-1, 0, 0);
+                break;
+            case Direction.RIGHT:
+                direction = new Vector3(1, 0, 0);
+                break;
+            default:
+                return null;
+        }
+
+        return null;//remove
+    }
 	
 	// Update is called once per frame
 	void Update () {
